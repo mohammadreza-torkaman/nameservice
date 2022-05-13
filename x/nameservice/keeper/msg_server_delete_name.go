50e5c5655812888b03a5e73ad1322d72a3c86dc8
mohammadreza torkaman <torkaman.mohammadreza@yahoo.com>
2022-05-13 10:28:00 +0430

    Delete-name message server implemented

--- x/nameservice/keeper/msg_server_delete_name.go
+++ x/nameservice/keeper/msg_server_delete_name.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/mohammadreza-torkaman/nameservice/x/nameservice/types"
@@ -9,9 +10,17 @@ import (
 
 func (k msgServer) DeleteName(goCtx context.Context, msg *types.MsgDeleteName) (*types.MsgDeleteNameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+	// Try getting name information from the store
+	whois, isFound := k.GetWhois(ctx, msg.Name)
+	// If a name is not found, throw an error
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Name doesn't exist")
+	}
+	// If the message sender address doesn't match the name owner, throw an error
+	if !(whois.Owner == msg.Creator) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+	}
+	// Otherwise, remove the name information from the store
+	k.RemoveWhois(ctx, msg.Name)
 	return &types.MsgDeleteNameResponse{}, nil
 }
