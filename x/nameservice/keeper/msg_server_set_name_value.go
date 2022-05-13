b3d0fb044b518ea0c0e4b60c19766ebae3ccddbf
mohammadreza torkaman <torkaman.mohammadreza@yahoo.com>
2022-05-13 10:28:11 +0430

    Set-name-value message server implemented

--- x/nameservice/keeper/msg_server_set_name_value.go
+++ x/nameservice/keeper/msg_server_set_name_value.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/mohammadreza-torkaman/nameservice/x/nameservice/types"
@@ -9,9 +10,21 @@ import (
 
 func (k msgServer) SetNameValue(goCtx context.Context, msg *types.MsgSetNameValue) (*types.MsgSetNameValueResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
+	// Try getting name information from the store
+	whois, _ := k.GetWhois(ctx, msg.Name)
+	// If the message sender address doesn't match the name owner, throw an error
+	if !(msg.Creator == whois.Owner) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect Owner")
+	}
+	// Otherwise, create an updated whois record
+	newWhois := types.Whois{
+		Index: msg.Name,
+		Name:  msg.Name,
+		Value: msg.Value,
+		Owner: whois.Owner,
+		Price: whois.Price,
+	}
+	// Write whois information to the store
+	k.SetWhois(ctx, newWhois)
 	return &types.MsgSetNameValueResponse{}, nil
 }
